9025acaf2afb429454f2d1602dca9c0e30ed2135
charr29 <charr29@gmail.com>
2015-11-03 17:41:39 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,14 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexYub = /^\/yubnub/
-      botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /^\odb/i; botDuck = /^\/duck/;
+      botRegex = /^\/cool guy/;  botRegexDL = /^\/DEP/i;botRegexSalt = /^\/salt/;botRegexYub = /^\/yubnub/
+      botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SCH/i; botODB = /^\odb/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/;
       botRegexSS = /^\/who is stu/; botRegexKK = /^\/kerm/; botRegexCC = /^\/cool/; botRegexGL = /^\/left/; botRegexPN = /^\/will antoine make playoffs/;
-      botRegexBG = /^\/bubblegum/;
+      botRegexBG = /^\/bubblegum/; botRegexSTP = /^\/stats/;botRegexSTT = /^\/tstats/;botRegexRL = /^\/rule/;
+      botRegexSTD = /^\/standings/;botRegexRK = /^\/rank/;botRegexCMD = /^\/command/; botRegexFA = /^\/free agents/;botRegexMRK = /^\/rookies/;
+      botRegexTD = /^\/trade/; botRegexSG = /^\/sign/; botRegexRLP = /^\/release/;botRegexSLR = /^\/salary/;
+      botRegexRLS = /^\/rules/; botRegexBRK = /^\/breakdown/; botRegexSwerve = /^\/swerve/;  botRegexJC = /^\/Geezus/; 
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
@@ -23,11 +26,107 @@ function respond() {
     postMessage("http://daddyleagues.com/nt/team/"+request.text.substring(5,8)+"/depthchart");
     this.res.end();
   } 
+  
+  else if(request.text && botRegexSTP.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/NT/stats/player");
+    this.res.end();
+  }  
+  
+  else if(request.text && botRegexSTT.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/NT/stats/team");
+    this.res.end();
+  } 
+  
+    
+  else if(request.text && botRegexRL.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/NT/stats/rules");
+    this.res.end();
+  } 
+    
+  else if(request.text && botRegexRLS.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/NT/stats/rules");
+    this.res.end();
+  } 
+    
+  else if(request.text && botRegexSTD.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/NT/stats/standings");
+    this.res.end();
+  } 
+    
+ else if(request.text && botRegexRK.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/NT/stats/standings/rankings");
+    this.res.end();
+  } 
+    
+  else if(request.text && botRegexFA.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/NT/players?name=&position=all&team=fa");
+    this.res.end();
+  } 
+    
+  else if(request.text && botRegexMRK.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/NT/players?name=&position=all&team=all&rookie=1");
+    this.res.end();
+  } 
+    
+  else if(request.text && botRegexTD.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/NT/frontoffice/trade");
+    this.res.end();
+  } 
+    
+  else if(request.text && botRegexSG.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/NT/frontoffice/sign");
+    this.res.end();
+  } 
+    
+  else if(request.text && botRegexRLP.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/NT/frontoffice/release");
+    this.res.end();
+  } 
+    
+  else if(request.text && botRegexSG.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/NT/frontoffice/salaries");
+    this.res.end();
+  } 
+    
+  else if(request.text && botRegexBRK.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/NT/frontoffice/rosterbreakdown");
+    this.res.end();
+  } 
+    
+  else if(request.text && botRegexSwerve.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://38.media.tumblr.com/3fe9310d30f7ab474a46499b549b8a55/tumblr_mjt8buEg3r1rvqgomo1_250.gif");
+    this.res.end();
+    
+  }
+  
+   else if(request.text && botRegexJC.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://memecrunch.com/meme/6GU2A/jesus-christ-kid/image.png");
+    this.res.end();
+    
+  }
   else if(request.text && botRegexSalt.test(request.text)) {
     this.res.writeHead(200);
     postMessage("https://i.imgur.com/B5BSVqH.png");
     this.res.end();
-  }  else if(request.text && botRegexCC.test(request.text)) {
+    
+  }  
+  
+  else if(request.text && botRegexCC.test(request.text)) {
     this.res.writeHead(200);
     postMessage("As cool as the other side of the pillow.");
     this.res.end();
