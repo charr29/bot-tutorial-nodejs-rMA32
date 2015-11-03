f0710205cb3cd7be8c0222cffa391534fd9f61a3
charr29 <charr29@gmail.com>
2015-11-03 10:34:38 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ function respond() {
   } 
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/nt/team"+request.text.substring(5,8)+"/depthchart");
+    postMessage("http://daddyleagues.com/nt/team/"+request.text.substring(5,8)+"/depthchart");
     this.res.end();
   } 
   else if(request.text && botRegexSalt.test(request.text)) {
